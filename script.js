5e04448ff2be12aa67dcb7c15e980b72c0444cbe
Janvi (Jéh) <cosmeratech@gmail.com>
2026-01-13 10:12:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// animating the static & making the animation occur only when we hover the card this can require js to modify the seed value over time or we can use animate element in html 
+
+// optional js :
+
+// const turbulence = document.querySelector("#noise feTurbulence");
+
+// document.querySelector("#card").onmouseenter = () => {
+//     setInterval(() => {
+//         turbulence.setAttribute("seed", (Math.random() * 1000));
+//     }, 10);  
+// }
